047529b5539eddacfcd3ee6221c57274fc277b12
ad3bay0 <byoextreme@hotmail.com>
2020-07-27 00:03:56 +0100

    feature: init swagger documentation

--- src/main/java/com/ad3bay0/gatest/controllers/CharacterController.java
+++ src/main/java/com/ad3bay0/gatest/controllers/CharacterController.java
@@ -3,8 +3,11 @@ package com.ad3bay0.gatest.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+
 @RestController
 @RequestMapping("/character")
+@Api(tags = "Character API")
 public class CharacterController {
     
 }
