03b6b860ac1cb976e375fd7d349b3f9b8b1da99e
ad3bay0 <byoextreme@hotmail.com>
2020-07-26 13:03:34 +0100

    chore: code base package setup

---
+++ src/main/java/com/ad3bay0/gatest/controllers/Location.java
@@ -0,0 +1,10 @@
+package com.ad3bay0.gatest.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Location {
+    
+}
