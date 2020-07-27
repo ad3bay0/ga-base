d5c990da7afd6e626a2e57db2261138aaa0eca6d
ad3bay0 <byoextreme@hotmail.com>
2020-07-27 22:16:29 +0100

    refactor: springboot app name

--- src/main/java/com/ad3bay0/gatest/GaTestApp.java
+++ src/main/java/com/ad3bay0/gatest/GaTestApp.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class DemoApplication {
+public class GaTestApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(GaTestApp.class, args);
 	}
 
 }
